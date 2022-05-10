1b7433c70540f77a6aa057112167d06a8dca1eb8
stevecyj <ycsteven.tsao@gmail.com>
2022-05-10 14:43:06 +0800

    [action]:deploy to heroku

--- connections/index.js
+++ connections/index.js
@@ -4,4 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DB).then(() => console.log('資料庫連接成功'));
+mongoose
+  .connect(DB)
+  .then(() => console.log('資料庫連接成功'))
+  .catch((err) => console.log(`${err.codeName} 資料庫連接失敗`));
